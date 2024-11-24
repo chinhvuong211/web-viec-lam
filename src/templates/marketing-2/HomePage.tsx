e3cdbdb140afb327d6249c4c7f27ff7501cf7344
ducnl <chinhvuong211@gmail.com>
2024-11-24 17:27:06 +0700

    restructure templete

--- src/templates/marketing-2/HomePage.tsx
+++ src/templates/marketing-2/HomePage.tsx
@@ -1,10 +1,60 @@
+import { Assets } from '@/assets';
+import MarketingAboutMe from '@/features/about-me/marketing/AboutMe';
 import { MarketingData } from '@/features/common/interface';
+import TestimonialSection from '@/features/home/marketing/TestimonialSection';
+
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 type Props = {
     data: MarketingData;
 };
 
-export default function MarketingStyle2HomePage(props: Props) {
+export default function Marketing2AboutMe(props: Props) {
     const { data } = props;
-    return <div>{data.aboutMe.name}</div>;
+    const {
+        aboutMe: { social },
+    } = data;
+
+    return (
+        <div>
+            <Header name={data.aboutMe.name} />
+            <MarketingAboutMe data={data} />
+
+            {/* Testimonial Section */}
+            <TestimonialSection
+                data={data.whatTheySaidAboutMe}
+                cardTheme={{
+                    background: '!bg-white shadow-xs',
+                    description: 'text-[#595959]',
+                    title: 'text-[#272626]',
+                    position: 'text-[#595959]',
+                }}
+                activeDotClassName={'bg-[#272626]'}
+                inactiveDotClassName={'bg-[#C3C3C3]'}
+            />
+            <Footer
+                data={{
+                    socials: [
+                        {
+                            icon: Assets.facebookIcon.src,
+                            link: social.facebookLink,
+                        },
+                        {
+                            icon: Assets.instagramIcon.src,
+                            link: social.instagramLink,
+                        },
+                        {
+                            icon: Assets.linkedInIcon.src,
+                            link: social.linkedinLink,
+                        },
+                        {
+                            icon: Assets.youtubeIcon.src,
+                            link: social.youtubeLink,
+                        },
+                    ],
+                }}
+            />
+        </div>
+    );
 }
--- src/templates/marketing-3/WorkDetailPage.tsx
+++ src/templates/marketing-3/WorkDetailPage.tsx
@@ -1,12 +1,35 @@
 import { MarketingData } from '@/features/common/interface';
+import TestimonialSection from '@/features/home/marketing/TestimonialSection';
 import MarketingWorkDetail from '@/features/work/detail/MarketingWorkDetail';
 import { ProjectItem } from '@/features/work/interface';
+
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 type Props = {
     data: MarketingData;
     slug: string;
 };
-export default function MarketingStyle3WorkDetailPage({ data, slug }: Props) {
+export default function Marketing3WorkDetailPage({ data, slug }: Props) {
     const projectDetail = data.projects.find((item) => item.id === slug) as ProjectItem;
-    return <MarketingWorkDetail projectDetail={projectDetail} />;
+
+    return (
+        <div>
+            <Header name={data.aboutMe.name} />
+            <MarketingWorkDetail projectDetail={projectDetail} />
+            {/* Testimonial Section */}
+            <TestimonialSection
+                data={data.whatTheySaidAboutMe}
+                cardTheme={{
+                    background: '!bg-white shadow-xs',
+                    description: 'text-[#595959]',
+                    title: 'text-[#272626]',
+                    position: 'text-[#595959]',
+                }}
+                activeDotClassName={'bg-[#272626]'}
+                inactiveDotClassName={'bg-[#C3C3C3]'}
+            />
+            <Footer />
+        </div>
+    );
 }
